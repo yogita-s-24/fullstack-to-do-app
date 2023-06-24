10f752a466dbfb0e7f2387fbaeba1ace23bbb841
yogitashete2412 <yogitashete24@gmail.com>
2023-06-24 17:47:41 +0530

    doc : backend server complete

--- server/index.js
+++ server/index.js
@@ -1,60 +1,108 @@
- import express from "express";
- import mongoose from "mongoose";
- import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
-
-// import Task from "./models/Task.js"
+import Task from "./models/Task.js";
 
 const app = express();
 app.use(express.json());
 
-async function connectmongoDB(){
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
-    if(conn){
+async function connectMongoDB() {
+    const conn = mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
         console.log("Connected to MongoDB");
     }
- }
-connectmongoDB();
+}
+connectMongoDB();
 
-// app.get('/health',(req,res)=>{
-//     res.json({
-//         success:true,
-//         message:'All good'
-//     })
-// })
+//Dumy API (basic server setup API)
+app.get("/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "All Good",
+    });
+});
 
-// //POST /task
+// POST /task
+app.post("/task", async (req, res) => {
+    const { title, description } = req.body;
 
-// app.post('/task',async(req,res)=>{
-//  const {title, description} = req.body;
- 
-// const newTask = new Task({
-//     title : title,
-//     description :description
-// })
+    const newTask = new Task({
+        title: title,
+        description: description,
+    });
 
-// const savedTask = await newTask.save();
+    const savedTask = await newTask.save();
 
-// res.json({
-//     success:true,
-//     message:'Task saved successfully.',
-//     data : savedTask
-// })
+    res.json({
+        success: true,
+        message: "Task saved successfully",
+        data: savedTask,
+    });
+});
 
-// });
+// Get /tasks   -  (for read all tasks)
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find();
+    res.json({
+        success: true,
+        message: "All tasks fetched successfully",
+        data: tasks,
+    });
+});
 
-// //GET / tasks
+// Get /task
+app.get("/task", async (req, res) => {
+    const taskId = req.query.taskId;
 
-// //GET / task
+    const task = await Task.findOne({ _id: taskId });
 
-// //DELETE / task / delete
+    if (!task) {
+        return res.json({
+            success: false,
+            message: "Task not found",
+            data: [],
+        });
+    }
+    res.json({
+        success: true,
+        message: "Task successfully fetched",
+        data: task,
+    });
+});
 
-// //PUT / task
+// POST/task/delete
+app.post("/task/delete", async (req, res) => {
+    const { taskId } = req.body;
 
+    await Task.deleteOne({
+        _id: taskId,
+    });
 
- const PORT = process.env.PORT || 5000;
+    res.json({
+        success: true,
+        message: "Task successfully deleted",
+    });
+});
 
-app.listen(PORT,()=>{
-    console.log("Listen on Port "+PORT)
-})
+// Put/task
+
+app.put("/task", async (req, res) => {
+    const { taskId, title, description } = req.body;
+
+    const task = await Task.updateOne({_id: taskId}, {
+         $set: {title: title, description: description} 
+    });
+
+    res.json({
+        success:true,
+        message:'Task successfully updated'
+    })
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+});
