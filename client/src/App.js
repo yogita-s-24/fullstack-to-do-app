dde3e8f33368fafa1451b14417c9fd955d90e308
yogitashete2412 <yogitashete24@gmail.com>
2023-06-25 14:03:21 +0530

    added usestate and useeffect varible and install axois

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
 import logo from './logo.svg';
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addTask = async()=>{
+    const {data} = await axios.post('/task',{
+      title: title,
+      description: description
+    })
+    alert(date?.message);
+    setTitle("");
+    setDescription("");
+  }
+
   return (
-   
     <>
       <h1 className="app-title">TO DO APP</h1>
       <div className="todo-container">
-        
+
       </div>
       <div className="todo-form-container">
-        <input type="text" className="todo-title" placeholder="Enter Your Title"/>
-        <input type="text" className="todo-description" placeholder="Enter Your Description"/>
-        <button className="todo-add">Add Task</button>
+        <input type="text" className="todo-title" placeholder="Enter Title" value={title} onChange={(e)=>{
+          setTitle(e.target.value);
+        }}/>
+
+        <input type="text" className="todo-description" placeholder="Enter Description" value={description} onChange={(e)=>{
+          setDescription(e.target.value);
+        }}/>
+
+        <button className="todo-add" onClick={addTask}>Add Task</button>
       </div>
     </>
   );
