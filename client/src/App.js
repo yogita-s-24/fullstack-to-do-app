d3a3ceb0cdf263ab298df3dd8d8c9f1984251708
yogitashete2412 <yogitashete24@gmail.com>
2023-06-27 07:20:52 +0530

    ref : install axios library

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
@@ -39,3 +38,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
